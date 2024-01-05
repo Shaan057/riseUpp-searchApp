660de359b40f995b78369b49dbf270273a3070f8
Roshan <141735478+Shaan057@users.noreply.github.com>
2024-01-05 18:44:04 +0530

    Loader component created

---
+++ src/components/Loader/index.js
@@ -0,0 +1,10 @@
+import { DNA } from 'react-loader-spinner'
+
+const Loader = () => (
+    <div className="loader-container">
+        <DNA type="TailSpin" color="#D81F26" height="50" width="50" />
+    </div>
+)
+
+export default Loader
+
