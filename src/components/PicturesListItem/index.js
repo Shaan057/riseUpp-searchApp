69553672b29ca7ee30764a5b1b129d400b3074e7
Roshan <141735478+Shaan057@users.noreply.github.com>
2024-01-05 21:42:17 +0530

    picture list component created

--- src/components/PicturesListItem/index.js
+++ src/components/PicturesListItem/index.js
@@ -1,10 +1,18 @@
 import './index.css'
 
 
-const PicturesListItem = () => {
-  return (
-    <li>1234</li>
-  )
+const PicturesListItem = ({ pictureData }) => {
+    const {
+        id, title, description, publishedAt, lastCollectedAt,
+        updatedAt, featured, totalPhotos, shareKey, tags,
+        links, user, coverPhoto, previewPhotos, likes } = pictureData
+
+    const { full, raw, regular, small, thumb, small_s3 } = coverPhoto.urls
+    return (
+        <li className='picture-list-item'>
+            <img className='picture' src={regular} alt={title} />
+        </li>
+    )
 }
 
 export default PicturesListItem
