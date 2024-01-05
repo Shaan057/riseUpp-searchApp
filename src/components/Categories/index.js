c0b7bf6210f7484862d46469e4d0a37e023cf33d
Roshan <141735478+Shaan057@users.noreply.github.com>
2024-01-05 22:22:20 +0530

    active tab added

--- src/components/Categories/index.js
+++ src/components/Categories/index.js
@@ -1,9 +1,16 @@
 import './index.css'
 
-const Categories = ({ category }) => {
+const Categories = ({ category, updateActiveTab, activeCategoryTab }) => {
+
+    const setActiveTab = () => {
+        updateActiveTab(category)
+    }
+    const isActiveTab = activeCategoryTab === category
+    const classStyle = isActiveTab ? 'category-button active-category-tab' : 'category-button'
+
     return (
         <li className='category-list-item'>
-            <button className='category-button' type='button'>{category}</button>
+            <button className={classStyle} type='button' onClick={setActiveTab}>{category}</button>
         </li>
     )
 }
