5e81e50f0b3cfdd2e0d76aaf3e0850938e18fcb3
Roshan <141735478+Shaan057@users.noreply.github.com>
2024-01-05 17:53:01 +0530

    added reducer

--- src/features/picturesSlice.js
+++ src/features/picturesSlice.js
@@ -7,25 +7,54 @@ const apiStatusConstants = {
     inProgress: 'IN_PROGRESS',
 }
 
+const categoriesList = [
+    'Mountains', 'Flowers', 'Cities',
+    'Beaches', 'Fruits', 'Animals',
+    'Cars', 'Bikes', 'Mobiles',
+    'Accessories', 'Laptops', 'Watches',
+]
 
 const initialState = {
-    picturesList:null,
+    picturesList: null,
+    categoriesList: [
+        'Mountains', 'Flowers', 'Cities',
+        'Beaches', 'Fruits', 'Animals',
+        'Cars', 'Bikes', 'Mobiles',
+        'Accessories', 'Laptops', 'Watches',
+    ],
+    activeCategory: categoriesList[0],
     apiStatus: apiStatusConstants.initial
 }
 
 
 
 export const pictureSlice = createSlice({
-    name:"picture",
+    name: "picture",
     initialState,
-    reducers:{
-        add:(state,action)=>{
-            state.picturesList=action.payload
+    reducers: {
+        setActiveCategory: (state, action) => {
+            state.activeCategory = action.payload
+        },
+        updatePicturesList: (state, action) => {
+            state.picturesList = action.payload
+        },
+        setApiStatus:(state,action)=>{
+            switch (action.payload) {
+                case apiStatusConstants.inProgress:
+                    state.apiStatus = apiStatusConstants.inProgress
+                case apiStatusConstants.success:
+                    state.apiStatus = apiStatusConstants.success
+                case apiStatusConstants.failure:
+                    state.apiStatus = apiStatusConstants.failure
+                default:
+                    state.apiStatus = apiStatusConstants.initial
+            }
+           
         }
     }
 })
 
 
-export const {add} = pictureSlice.actions
+export const { setActiveCategory, updatePicturesList, setApiStatus } = pictureSlice.actions
 
 export default pictureSlice.reducer
