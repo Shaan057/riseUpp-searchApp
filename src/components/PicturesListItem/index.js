4aa37f2355f946c43fd4985aa304c4edcd3c9d6a
Roshan <141735478+Shaan057@users.noreply.github.com>
2024-01-05 22:25:34 +0530

    mouse events added

--- src/components/PicturesListItem/index.js
+++ src/components/PicturesListItem/index.js
@@ -1,17 +1,27 @@
 import './index.css'
 
 
-const PicturesListItem = ({ pictureData }) => {
+const PicturesListItem = ({ pictureData, onHoverIn, onMouseHoverOut }) => {
     const {
         id, title, description, publishedAt, lastCollectedAt,
         updatedAt, featured, totalPhotos, shareKey, tags,
         links, user, coverPhoto, previewPhotos, likes } = pictureData
 
     const { full, raw, regular, small, thumb, small_s3 } = coverPhoto.urls
+
+    const handleMouseEnter = () => {
+        onHoverIn()
+    }
+    const handleMouseLeave = () => {
+        onMouseHoverOut()
+    }
     return (
-        <li className='picture-list-item'>
-            <img className='picture' src={regular} alt={title} />
-        </li>
+        <>
+            <li className='picture-list-item' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+                <img className='picture' src={regular} alt={title} />
+            </li>
+        </>
+
     )
 }
 
