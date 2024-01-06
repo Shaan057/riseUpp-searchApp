c2f3b9a54079036e6b2b6e5925cfc434b7748a73
Roshan <141735478+Shaan057@users.noreply.github.com>
2024-01-06 11:47:43 +0530

    popup functionality added

--- src/components/PicturesListItem/index.js
+++ src/components/PicturesListItem/index.js
@@ -1,27 +1,46 @@
 import './index.css'
 
+import { useState } from 'react'
 
-const PicturesListItem = ({ pictureData, onHoverIn, onMouseHoverOut }) => {
+
+const PicturesListItem = (props) => {
+    const [isClicked, setClick] = useState(false)
+
+    const { pictureData, onHoverIn, onMouseHoverOut, isHoveredOn } = props
     const {
         id, title, description, publishedAt, lastCollectedAt,
         updatedAt, featured, totalPhotos, shareKey, tags,
         links, user, coverPhoto, previewPhotos, likes } = pictureData
-
+    const date = new Date(publishedAt).toLocaleDateString()
     const { full, raw, regular, small, thumb, small_s3 } = coverPhoto.urls
 
-    const handleMouseEnter = () => {
-        onHoverIn()
-    }
-    const handleMouseLeave = () => {
-        onMouseHoverOut()
-    }
-    return (
-        <>
-            <li className='picture-list-item' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-                <img className='picture' src={regular} alt={title} />
-            </li>
-        </>
 
+
+
+
+    const handleMouseEnter = () => {
+        onHoverIn(id)
+    }
+
+    const handleMouseLeave = () => {
+        onMouseHoverOut(0)
+    }
+
+    const popup = () => <>
+        <div className={isHoveredOn === id ? 'popup-container show-popup' : 'popup-container hide-popup'}>
+            <p className='popup-text'><span className='spanned'>Category :</span> <br />{title}</p>
+            <p className='popup-text'><span className='spanned'>Description :</span> <br /> This picture is beautiful</p>
+            <p className='popup-text'><span className='spanned'>Published On :</span> <br />{date}</p>
+        </div>
+    </>
+
+    return (
+        <li className='picture-list-item' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} >
+            <a href={full} rel="noopener noreferrer">
+                <img className='picture' src={regular} alt={title} />
+                {popup()}
+            </a>
+        </li>
     )
 }
 
