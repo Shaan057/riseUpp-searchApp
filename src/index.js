34cc38c77bf5b8da265299a2daa4da300b1720a6
Roshan <141735478+Shaan057@users.noreply.github.com>
2024-01-18 16:25:32 +0530

    env variables

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Auth0Provider 
-    domain="dev-co2414dequnzswv6.us.auth0.com"
-    clientId="GzFdEcP46TcssUCIXJqPUoKmC44flh9x"
+  <Auth0Provider
+    domain={process.env.REACT_APP_AUTH0_DOMAIN}
+    clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
